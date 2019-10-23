cee20f7ddb310c0b6079caadab1e3fb68ccf676f
Michael Lustfield <michael@lustfield.net>
2019-10-22 22:44:22 -0500

    Tag another point release (1.8); minor code cleanup

--- tdc.c
+++ tdc.c
@@ -142,7 +142,7 @@ void get_params(Display *display, int argc, char *argv[]) {
 
   /* check for version request */
   if (XrmGetResource(database, "tdc.version", "tdc.version", &type, &xrmval) == True) {
-    printf("tdc 1.7\n");
+    printf("tdc 1.8\n");
     exit(0);
   }
 
